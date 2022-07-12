382eb6b8668f169534ae18fa26aab436f5c2e938
MominNurein <Shaweeshnurein7@gmail.com>
2022-07-12 17:48:49 +0200

    small fix

--- src/components/OcrSearch.vue
+++ src/components/OcrSearch.vue
@@ -1,69 +1,74 @@
+sl
 <template>
-	<!-- <div> -->
-	<ion-header>
-		<ion-toolbar>
-			<ion-buttons slot="end">
-				<ion-buttons color="medium" @click="dismiss">الغاء</ion-buttons>
-			</ion-buttons>
-			<ion-title slot="start">بحث عن طريق الصورة</ion-title>
-		</ion-toolbar>
-	</ion-header>
-	<ion-content :fullscreen="true">
-		<div class="h-full w-full p-4">
-			<div class="h-full w-full p-4 relative">
-				<!-- <input
+	<div>
+		<ion-header>
+			<ion-toolbar>
+				<ion-buttons slot="end">
+					<ion-buttons color="medium" @click="dismiss"
+						>الغاء</ion-buttons
+					>
+				</ion-buttons>
+				<ion-title slot="start">بحث عن طريق الصورة</ion-title>
+			</ion-toolbar>
+		</ion-header>
+		<ion-content :fullscreen="true">
+			<div class="h-full w-full p-4">
+				<div class="h-full w-full p-4 relative">
+					<!-- <input
 					type="file"
 					id="image-input"
 					class="custom-file-input"
 					accept="image/jpeg, image/png, image/jpg"
 				/><br /><br /> -->
-				<br />
-				<ion-button
-					v-if="imageUrl != ''"
-					color="danger"
-					class="cross"
-					@click="removeImage"
-					><ion-icon :icon="icons.trash"></ion-icon
-				></ion-button>
-				<img
-					id="medicine-image"
-					:src="imageUrl ? imageUrl : '/noimage.png'"
-					alt="The choosen image will be displayed here ."
-				/>
+					<br />
+					<ion-button
+						v-if="imageUrl != ''"
+						color="danger"
+						class="cross"
+						@click="removeImage"
+						><ion-icon :icon="icons.trash"></ion-icon
+					></ion-button>
+					<img
+						ref="myImage"
+						id="medicine-image"
+						:src="imageUrl ? imageUrl : '/noimage.png'"
+						alt="The choosen image will be displayed here ."
+					/>
 
-				<br />
+					<br />
 
-				<p class="text-center py-2" id="hint">
-					الرجاء ادخال صورة واضحه للوصفه الطبيه او صورة لصندوق الدواء
-				</p>
+					<p class="text-center py-2" id="hint">
+						الرجاء ادخال صورة واضحه للوصفه الطبيه او صورة لصندوق
+						الدواء
+					</p>
 
-				<div class="buttonsw-full">
-					<button
-						class="bg-blue-600 flex flex-col items-center justify-center px-4 py-8 text-white w-full rounded"
-						@click="takePicture()"
-					>
-						<ion-icon
-							class="w-8 h-8"
-							:icon="icons.cameraOutline"
-						></ion-icon>
-						<span class="py-2 text-xl"> اختيار صورة </span>
-					</button>
-					<button
-						class="bg-lime-600 text-white p-4 w-full rounded absolute left-0 bottom-2"
-						@click="recognizeImage()"
-					>
-						بحث
-					</button>
+					<div class="buttonsw-full">
+						<button
+							class="bg-blue-600 flex flex-col items-center justify-center px-4 py-8 text-white w-full rounded"
+							@click="takePicture()"
+						>
+							<ion-icon
+								class="w-8 h-8"
+								:icon="icons.cameraOutline"
+							></ion-icon>
+							<span class="py-2 text-xl"> اختيار صورة </span>
+						</button>
+						<button
+							class="bg-lime-600 text-white p-4 w-full rounded absolute left-0 bottom-2"
+							@click="recognizeImage()"
+						>
+							بحث
+						</button>
+					</div>
 				</div>
-			</div>
 
-			<!-- result section -->
-			<!-- <h3>Result :</h3>
+				<!-- result section -->
+				<!-- <h3>Result :</h3>
 
 				<div id="result"></div> -->
-		</div>
-	</ion-content>
-	<!-- </div> -->
+			</div>
+		</ion-content>
+	</div>
 </template>
 
 <script>
@@ -79,7 +84,7 @@ import {
 	modalController,
 } from '@ionic/vue';
 import { cameraOutline, trash } from 'ionicons/icons';
-import { createWorker, OEM, PSM } from 'tesseract.js';
+import { createWorker } from 'tesseract.js';
 // import { Plugin } from '@capacitor/core';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
@@ -89,6 +94,7 @@ export default {
 		return {
 			imageUrl: '',
 			recognizedText: '',
+			cropper: {},
 			icons: {
 				cameraOutline,
 				trash,
@@ -106,28 +112,20 @@ export default {
 	},
 	methods: {
 		async takePicture() {
-			const image = await Camera.getPhoto({
-				quality: 100,
+			let image = await Camera.getPhoto({
+				quality: 90,
+				allowEditing: true,
 				resultType: CameraResultType.Uri,
 				source: CameraSource.Prompt,
-				saveToGallery: false,
-				webUseInput: CameraSource.Prompt,
-				allowEditing: true,
-
-				// allowEdit: true,
+				presentationStyle: 'popover',
+				webUseInput: true,
 			});
-
-			// image.webPath will contain a path that can be set as an image src.
-			// You can access the original file using image.path, which can be
-			// passed to the Filesystem API to read the raw data of the image,
-			// if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
 			console.log(image);
 			this.imageUrl = image.webPath;
 			document.querySelector('#medicine-image').src = this.imageUrl;
-			// document.querySelector('#result').innerHTML = this.imageUrl;
-			console.log(this.imageUrl);
-		},
 
+			document.querySelector('#result').innerHTML = this.imageUrl;
+		},
 		async recognizeImage() {
 			let loading = await loadingController.create({
 				spinner: 'bubbles',
@@ -136,14 +134,14 @@ export default {
 
 			loading.present();
 			const worker = createWorker({
-				logger: console.log(logger),
+				logger: (p) => console.log(p),
 			});
 			(async () => {
 				await worker.load();
 				await worker.loadLanguage('eng');
-				await worker.initialize('eng', OEM.DEFAULT);
+				await worker.initialize('eng');
 				await worker.setParameters({
-					tessedit_pageseg_mode: PSM.SPARSE_TEXT,
+					// tessedit_pageseg_mode: PSM.SPARSE_TEXT_OSD,
 					tessedit_char_whiltelist:
 						'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',
 					tessedit_char_blacklist: `/;][=-+_"'-__--{}:~!@#$%^&*()`,
